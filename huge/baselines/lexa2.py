b070f758401402b84c5dfaba02bce8974e911f30
Marcel Torne Villasevil <marceltovi@gmail.com>
2023-08-02 22:59:44 -0400

    Add kitchen

--- huge/baselines/lexa2.py
+++ huge/baselines/lexa2.py
@@ -518,9 +518,10 @@ class LEXA:
         return self.densities[tuple(idx)] 
 
     def add_visited_states(self, achieved_states):
-        idx = self.get_grid_cell(achieved_states)
         import IPython
         IPython.embed()
+        idx = self.get_grid_cell(achieved_states)
+
         self.densities[tuple(idx)] += 1
   
     def get_grid_cell(self, achieved_states):
