ce506311dbaa1d8fb5763cb0256a33f4345d0157
Marcel Torne Villasevil <marceltovi@gmail.com>
2023-08-07 10:30:58 -0400

    Add hyperparameter tuning

--- launch_pebble_og.py
+++ launch_pebble_og.py
@@ -436,7 +436,6 @@ def main():
     parser.add_argument("--action_std",type=float, default=None)
     parser.add_argument("--max_path_length",type=int, default=None)
     parser.add_argument("--segment",type=int, default=None)    
-    parser.add_argument("--max_path_length",type=int, default=None)
     parser.add_argument("--num_seed_steps",type=int, default=None)
 
     args = parser.parse_args()
