8120e6444b1cf7103cc4daa76789ab3e7c55e466
Marcel Torne Villasevil <marceltovi@gmail.com>
2023-08-07 16:41:43 -0400

    Add ant maze

--- huge/envs/ant_maze.py
+++ huge/envs/ant_maze.py
@@ -244,9 +244,12 @@ class AntMazeIntermediate():
     return goal_state
 
   def render_image(self):
+    return np.zeros((64,64,3))
     return self._env.render(mode="rgb_array", width=64, height=64)
 
   def render(self, mode='rgb_array', width=480, height=64, camera_id=0):
+      return np.zeros((64,64,3))
+      
       return self._env.render(mode=mode)
    
   @property
