dac49faa1ec5761af456d251869cebc927485e44
Marcel Torne Villasevil <marceltovi@gmail.com>
2023-08-08 15:48:59 -0400

    Add HP human

--- launch_main.py
+++ launch_main.py
@@ -219,6 +219,7 @@ if __name__ == "__main__":
     parser.add_argument("--explore_episodes",type=int, default=None)
     parser.add_argument("--max_path_length",type=int, default=None)
     parser.add_argument("--repeat_previous_action_prob",type=float, default=None)
+    parser.add_argument("--network_layers", type=str, default=None)
     parser.add_argument("--task_config", type=str, default=None)
     parser.add_argument("--train_goal_selector_freq",type=int, default=None)
     parser.add_argument("--goal_selector_num_samples",type=int, default=None)
